3a5922a844bcc4ecf27c738e491f6da76fd0f175
wrs <wrs@wrsaunders.co.uk>
2020-10-20 17:45:38 +0100

    initial wrs experiments

---
+++ source/data_io.py
@@ -0,0 +1,33 @@
+import numpy as np
+import glob
+import ctypes
+import os
+
+
+def get_data_arrays(directory):
+    """
+    Load the pwm values and accelerometer values from a directory as int16_t
+    """
+
+    files = glob.glob(os.path.join(directory, "*"))
+    accel_values = np.array([np.fromfile(fx, dtype=ctypes.c_int16).reshape((-1, 3)) for fx in files])
+    pwm_values = np.array([int(fx.split("_")[-1]) for fx in files], dtype=ctypes.c_int16)
+
+    return pwm_values, accel_values
+
+
+def get_datasets(directory, train_ratio=0.9):
+    """
+    Returns train_pwm_values, train_accel_values, test_pwm_values and
+    test_accel_values from a directory.
+    """
+    pwm_values, accel_values = get_data_arrays(directory)
+
+    N = pwm_values.shape[0]
+    N_train = int(train_ratio * N)
+    indices = np.random.permutation(N)
+
+    train_indices = indices[:N_train]
+    test_indices = indices[N_train:]
+
+    return pwm_values[train_indices], accel_values[train_indices, :, :], pwm_values[test_indices], accel_values[test_indices, :, :]
---
+++ source/main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import data_io
+import ctypes
+import tensorflow as tf
+from tensorflow.keras import Model, Sequential, Input
+from tensorflow.keras.layers import Activation, Dense, Embedding, GlobalAveragePooling1D, Lambda, Flatten
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
+
+if __name__ == "__main__":
+
+    # pwm_values, accel_readings = data_io.get_data_arrays('../data/raw')
+    train_pwm_values, train_accel_values, test_pwm_values, test_accel_values = data_io.get_datasets("../data/raw")
+    
+    train_accel_values = train_accel_values / 255.0
+    train_accel_values = np.abs(np.fft.rfft(train_accel_values, axis=1))
+    train_accel_values = np.sum(train_accel_values, axis=-1)
+    train_accel_values = np.array(train_accel_values, dtype="float32")
+
+    model_input = Input(shape=train_accel_values.shape[1], dtype="float32")
+
+    model = Sequential(
+        (
+            model_input,
+            Dense(1024, activation="relu"), 
+            Dense(256, activation="relu"), 
+            Dense(1, activation="relu"),
+        )
+    )
+
+    model.summary()
+
+    import pdb;pdb.set_trace()
+    model.compile(
+        optimizer="adam", 
+        loss= tf.keras.losses.MeanSquaredError(),
+        metrics=["accuracy"]
+    )
+
+    model.fit(train_accel_values, train_pwm_values)
+    
+
+    import pdb;pdb.set_trace()
