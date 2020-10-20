2c7beadeaee69d0f45933ac9fcce92475dc8a629
Will Saunders <wrs@wrsaunders.co.uk>
2020-10-21 00:33:51 +0100

    validation data at epoch gives sad face

--- source/main_hist.py
+++ source/main_hist.py
@@ -45,18 +45,19 @@ if __name__ == "__main__":
     # pwm_values, accel_readings = data_io.get_data_arrays('../data/raw')
     train_pwm_values, train_accel_values, test_pwm_values, test_accel_values = data_io.get_datasets("../data/raw")
     
-    nbins = 256
+    nbins = 1024
     preprocessor = Preproccessor(train_accel_values, nbins)
     
     pp_train_accel_values = preprocessor.process_block(train_accel_values)
+    pp_test_accel_values = preprocessor.process_block(test_accel_values)
 
     model_input = Input(shape=nbins, dtype="int32")
 
     model = Sequential(
         (
             model_input,
+            Dense(512, activation="relu"), 
             Dense(256, activation="relu"), 
-            Dense(128, activation="relu"), 
             Dense(1, activation="relu"),
         )
     )
@@ -70,7 +71,7 @@ if __name__ == "__main__":
         metrics=["accuracy"]
     )
 
-    model.fit(pp_train_accel_values, train_pwm_values, epochs=10000)
+    model.fit(pp_train_accel_values, train_pwm_values, epochs=10000, validation_data=(pp_test_accel_values, test_pwm_values))
     
 
     import pdb;pdb.set_trace()
